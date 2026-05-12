f18e703d39ebdbd431a8e78d27bda1a89f6600f3
Benjamín Soto <soto619ft@gmail.com>
2026-05-12 08:50:35 -0400

    Delete clinica-front directory

--- clinica-front/clinica-front/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { RegistroComponent } from './components/registro/registro';
-import { MenuComponent } from './components/menu/menu';
-
-export const routes: Routes = [
-  { path: 'login', component: RegistroComponent },
-  { path: 'menu', component: MenuComponent },
-  { path: '', redirectTo: '', pathMatch: 'full' }
-];
--- clinica-front/clinica-front/src/app/components/landing/landing.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-landing',
-  imports: [],
-  templateUrl: './landing.html',
-  styleUrl: './landing.css',
-})
-export class Landing {}
